164de235f71b02b027b4eea6ba69ceb7c1cef41e
Ashgatchat <Ashleighjg@gmail.com>
2024-07-07 23:57:44 -0400

    adding js code for various functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,46 @@ const cityButtonsEl = document.querySelector('#city-buttons');
 const cityInputEl = document.querySelector('#cityname');
 const stateInputEl = document.querySelector('#statename');
 const currentContainerEl = document.querySelector('#current-container');
+const currentHeaderEl = document.querySelector('#current-header');
+//const currentTempEl = document.querySelector('#current-temp');
+//const currentHumidityEl = document.querySelector('#current-humidity');
+//const currentWindEl = document.querySelector('#current-wind');
 const futureContainerEl = document.querySelector('#future-container');
 const searchTerm = document.querySelector('#city-search-term');
 
 
 
-const rendercitiesfromStorage = function () {
-    let cities = JSON.parse(localStorage.getItem('cities'));
 
+const rendercitiesfromStorage = function () {
+    const storedCities = JSON.parse(localStorage.getItem('cities'));
+    let cities;
     // If there are no cities in localStorage, initialize an empty array
-    if (!cities) {
-        cities = [];
+    if (storedCities) {
+        cities = storedCities;
     }
+    else { cities=[];}
     return cities;
     };
 
-const getLatLon = function () {
-    const cityName = cityInputEl.value.trim();
-    const stateName = stateInputEl.value.trim();
+const getLonLat = function (cityName,stateName) {
+    //const cityName = cityInputEl.value.trim();
+    //const stateName = stateInputEl.value.trim();
     
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},US&limit=1&appid=8ddc1d4b97e0f258d62f7e1d5eccd92f`;
     
-    fetch(apiUrl).then(function (response) {
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},US&appid=8ddc1d4b97e0f258d62f7e1d5eccd92f&units=imperial`;
+    
+     fetch(apiUrl).then(function (response) {
         if (response.ok) {
-          response.json().then(function (data) {
+          return response.json()
+          .then(function (data) {
             console.log(data.main.temp, data.main.humidity, data.wind.speed);
             console.log(data.coord);
-            return(data.coord);
+            displayCurrentWeather(data);
+            getFutureWeather(data);
           });
         }
-});
+        });
 };
 
 const formSubmitHandler = function (event) {
@@ -41,12 +51,12 @@ const formSubmitHandler = function (event) {
     const cityName = cityInputEl.value.trim();
     const stateName = stateInputEl.value.trim();
     
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},US&limit=1&appid=8ddc1d4b97e0f258d62f7e1d5eccd92f`;
+    //const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},US&limit=6&appid=8ddc1d4b97e0f258d62f7e1d5eccd92f&units=imperial`;
 
       
-    if (cityName,stateName) {
-      
-        getLatLon();
+    if (cityName&&stateName) {
+        
+        getLonLat(cityName,stateName);
 
     } else {
       alert('Please enter a City name');
@@ -90,83 +100,117 @@ const formSubmitHandler = function (event) {
 
     }; 
 
-  
+           
+ const displayCurrentWeather = function (data) {
+ 
+    const temp = data.main.temp;
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed;
+    const now = dayjs();
+    const cityName = data.name;
 
-  const displayWeather = function () {
-    const cityName = cityInputEl.value.trim();
-    const stateName = stateInputEl.value.trim();
+
+    const currentCard = $('<div>').addClass('card');
+    const cardHeader = $('<h3>').addClass('card-header').text(cityName, now);
+        const cardBody = $('<div>').addClass('card-body');
+        const currentTemp = $('<p>').addClass('card-text').text(temp);
+        const currentHumidity = $('<p>').addClass('card-text').text(humidity);
+        const currentWind = $('<p>').addClass('card-text').text(windSpeed);
+        const cardIcon = $('<p>').addClass('card-icon');
+
+        cardBody.append(currentTemp, currentHumidity, currentWind, cardIcon);
+        currentCard.append(cardHeader,cardBody);
+        currentContainerEl.append(currentCard);
+    
+        return currentContainerEl;
+    
+  };
+
+  const getFutureWeather = function (data) {
     const lat = data.coord.lat;
     const lon = data.coord.lon;
+
+
+    const queryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=8ddc1d4b97e0f258d62f7e1d5eccd92f&units=imperial`;
+   
+    return fetch(queryUrl)
+    .then(function (response) {
+       if (response.ok) {
+       return response.json().then(function (data) {
+        console.log(data);
+        displayFutureWeather(data);
+        
+       });
+    } else {
+        alert(`Error:${response.statusText}`);
+      };
+      });
+
+  }
+
+
+  const displayFutureWeather = function (data) {
+
+
+    console.log(data.list[4]);
+    console.log(data.list[12]);
+    console.log(data.list[20]);
+    console.log(data.list[28]);
+    console.log(data.list[36]);
+
+    //let dayArray = [data.list[4], data.list[12], data.list[20], data.list[28], data.list[36]]
+
+    const day1 = data.list[4];
+    const day2 = data.list[12];
+    const day3 = data.list[20];
+    const day4 = data.list[28];
+    const day5 = data.list[36];
+
+    let dayArray = [day1, day2, day3, day4, day5]
+
+    for (let i = 0; i < dayArray.length; i++) {
+      console.log(dayArray[i]);
     
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=8ddc1d4b97e0f258d62f7e1d5eccd92f`;
-
-      
-    if (cityName,stateName) {
-      
-        fetch(apiUrl).then(function (response) {
-            if (response.ok) {
-              response.json().then(function (data) {
-                return(data);
-              });
-            } else {
-                alert(`Error:${response.statusText}`);
-              };
-          });
-      }; 
-        //createCard();
-
-
-
     };
-
-
-
-
-function createCard () {
-    //Create a new card element and add the classes `card`, `task-card`, `draggable`, and `my-3`. Also add a `data-task-id` attribute and set it to the Task id.
-    const card = $('<div>')
+  }
+      /*const temp = dayArray[i].main.temp;
+      const humidity = dayArray[i].main.humidity;
+      const windSpeed = dayArray[i].wind.speed;
+      const now = dayArray[i].dt_txt;
+      
+      
+ 
+      //Create a new card element and add the classes `card`.
+      const futureCard = $('<div>')
       .addClass('card  my-3');
-    // Create a new card header element and add the classes `card-header` and `h4`. Also set the text of the card header to the project name.
-    const cardHeader = $('<div>').addClass('card-header h4').text(list.dt_txt);
-    // Create a new card body element and add the class `card-body`.
-    const cardBody = $('<div>').addClass('card-body');
-    // Create a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project type.
-    const cardTemp = $('<p>').addClass('card-text').text(list.main.temp);
-    // Create a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project due date.
-    const cardHumidity = $('<p>').addClass('card-text').text(list.main.humidity);
-    // Create a new paragraph element and add the class `card-text`. Also set the text of the paragraph to the project status.
-    const cardWindspeed = $('<p>').addClass('card-text').text(list.wind.speed);
-    // Create a new paragraph element and add the class
-    const cardIcon = $('<p>').addClass('card-icon');
+      // Create a new card header element and add the classes `card-header` and `h4`. 
+      const cardHeaderF = $('<div>').addClass('card-header h4').text(now);
+      // Create a new card body element and add the class `card-body`.
+      const cardBodyF = $('<div>').addClass('card-body');
+      // Create a new paragraph element and add the class `card-text`. 
+      const cardTemp = $('<p>').addClass('card-text').text(temp);
+      // Create a new paragraph element and add the class `card-text`. 
+      const cardHumidity = $('<p>').addClass('card-text').text(humidity);
+      // Create a new paragraph element and add the class `card-text`. 
+      const cardWindspeed = $('<p>').addClass('card-text').text(windSpeed);
+      // Create a new paragraph element and add the class
+      const cardIcon = $('<p>').addClass('card-icon');
 
-  
-  
-    //Append the weather card data to the card body.
-  // Append the card header and card body to the card.
-    cardBody.append(cardTemp, cardHumidity, cardWindspeed, cardIcon);
-    card.append(cardHeader, cardBody);
-  
-    
-  
-    // ? Return the card so it can be appended 
-    return card;
-    currentContainerEl.append(card);
-  };
-    
- 
 
- 
-  
 
-  const displayCurrentWeather = function () {
-   
-    
-  };
+      //Append the weather card data to the card body.
+      // Append the card header and card body to the card.
+      cardBodyF.append(cardTemp, cardHumidity, cardWindspeed, cardIcon);
+      futureCard.append(cardHeaderF, cardBodyF);
 
-  const displayFutureWeather = function () {
-   
-    
-  };
+
+
+// ? Return the card so it can be appended 
+      return futureCard;
+      futureContainerEl.append(futureCard);
+    };
+              
+*/
 
 
 
